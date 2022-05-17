d363a569322997bde5162714d496c6f169c9dd9e
Jason Manson-Hing <jason.mansonhing@gmail.com>
2022-05-17 04:23:52 -0400

    working form w/ input and select + submit

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,19 +1,45 @@
 import React from 'react';
 
-const Input = (({ placeholder, type, register, ...rest }) => (
-  <input type={type} className="
-    mt-1
-    block
-    w-full
-    rounded-md
-    border-gray-300
-    shadow-sm
-    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-    "
-    placeholder={placeholder} {...register} />
+const Input = (({ value, label, type, register, ...rest }) => (
+  <>
+    <label htmlFor={value} className="text-gray-700 block">{label}</label>
+    <input type={type} className="
+      mt-2
+      mb-4
+      block
+      w-full
+      rounded-md
+      border-gray-300
+      shadow-sm
+      focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+      "
+      {...register} />
+    </>
+));
+
+const Select = React.forwardRef(({ value, label, options, register, ...rest }, ref) => (
+  <>
+    <label htmlFor={value} className="text-gray-700 block">{label}</label>
+    <select className="
+      mt-2
+      mb-4
+      block
+      w-full
+      rounded-md
+      border-gray-300
+      shadow-sm
+      focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+      "
+      {...register}>
+        {options.map(option => (
+          <option value={option.value} key={option.label}>{option.label}</option>
+        ))}
+      </select>
+    </>
 ));
 
 export {
   Input,
+  Select,
 }
 
