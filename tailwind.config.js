50dd555de156f2e502102ec94b5cd8b8354c0a3a
Jason Manson-Hing <jason.mansonhing@gmail.com>
2022-05-17 01:59:54 -0400

    updated tailwind config, working reacthookform w/ validation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  purge: {
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-  },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
