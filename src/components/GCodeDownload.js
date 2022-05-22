d69ff94597f36f624c4b5c3a86f0b1c4f8bb701d
Jason Manson-Hing <jason.mansonhing@gmail.com>
2022-05-22 05:14:38 -0400

    fixed filename input

--- src/components/GCodeDownload.js
+++ src/components/GCodeDownload.js
@@ -1,10 +1,13 @@
 import generateGcode from '../utils/generateGcode';
+import { StyledInput } from './Inputs';
 import { useStore } from "../stores/store";
 
 
 export default function GCodeDownload() {
     const options = useStore((state) => state.options);
-    const fileName = useStore((state) => state.fileName).replace(/[/\\?%*:|"<> ]/g, '_');
+    const fileName = useStore((state) => state.fileName);
+    const setFileName = useStore((state) => state.setFileName);
+
     const gcode = generateGcode(options);
     const url = URL.createObjectURL(new Blob([gcode], {
         type: "text/plain"
@@ -14,13 +17,14 @@ export default function GCodeDownload() {
         const element = document.createElement("a");
         element.href = url;
         const datestamp = new Date().toISOString().replace(/T/, '_').replace(/:/g, '-').replace(/\..+/, '')
-        element.download = `${fileName ? fileName + '-' : ''}${datestamp}.gcode`;
+        element.download = `${fileName ? fileName.replace(/[/\\?%*:|"<> ]/g, '_') + '-' : ''}${datestamp}.gcode`;
         document.body.appendChild(element);
         element.click();
       };
 
     return (
         <> 
+            <StyledInput type="text" value="fileName" defaultValue={fileName} label="File Name (Optional)" handleChange={(e) => {setFileName(e.target.value)}} />
             <button className="w-full mt-4 h-12 px-6 text-white font-bold transition-colors duration-150 bg-indigo-700 rounded-md focus:shadow-outline hover:bg-indigo-800" onClick={downloadGcode}>Download GCode</button>
             <p className="mb-0 text-center">
                 <a className="text-xs text-neutral-300 transition-colors duration-150 hover:text-neutral-500 text-center underline" href="https://github.com/iFallUpHill/flow-calculator" target="_blank" rel="noreferrer noopener nofollow">View Source on GitHub</a>
