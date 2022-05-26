6655da3b085c872872824a46da8248a37fd3230f
Eric Karschner <erickarschner@gmail.com>
2022-05-26 00:07:23 -0400

    Add util function to evaluate template expressions

---
+++ src/tests/replaceTemplateVars.test.js
@@ -0,0 +1,33 @@
+import { replaceTemplateVars } from "../utils/replaceTemplateVars";
+
+describe("replaceTemplateVars", () => {
+  it("correctly evaluates mathematical expressions", () => {
+    expect(replaceTemplateVars("${5 + 6 - 3}", {})).toBe("8");
+  });
+
+  it("correctly substitutes properties from a configuration object", () => {
+    expect(replaceTemplateVars("${var}", { var: 5 })).toBe("5");
+  });
+
+  it("respects order of operations", () => {
+    expect(replaceTemplateVars("${5 + 4 *(var-3)}", { var: 5 })).toBe("13");
+  });
+
+  it("handles absolute values", () => {
+    expect(replaceTemplateVars("${Math.abs(-1)}", {})).toBe("1");
+  });
+
+  it("can parse more complicated expressions", () => {
+    const string =
+      "output.push(G0 X${bedWidth - Math.abs(bedMargin)} Y${maxBedLength - Math.abs(bedMargin)} ; Move to Corner);";
+    const config = {
+      bedWidth: 250,
+      bedMargin: 5,
+      maxBedLength: 210,
+    };
+
+    expect(replaceTemplateVars(string, config)).toBe(
+      "output.push(G0 X245 Y205 ; Move to Corner);"
+    );
+  });
+});
---
+++ src/utils/replaceTemplateVars.js
@@ -0,0 +1,54 @@
+const templateRegex = /\$\{([^}]+)\}/gi;
+
+const operators = {
+  "+": (x, y) => x + y,
+  "*": (x, y) => x * y,
+  "-": (x, y) => x - y,
+  "/": (x, y) => x / y,
+};
+
+const parenthesesRegex = /(Math\.abs)?\(([^)]+)\)/gi;
+
+const binaryOpRegex = (op) => new RegExp(`(\\w+)\\s*([${op}])\\s*(\\w+)`, 'g');
+
+const parseTerm = (term, obj) => obj[term] || parseInt(term);
+
+function evaluateTemplateExpression(expression, obj) {
+  console.log(`original: ${expression}`)
+
+  console.log(expression.match(parenthesesRegex));
+  while (expression.match(parenthesesRegex)) {
+    expression = expression.replaceAll(parenthesesRegex, (_, abs, exp) =>
+      abs
+        ? Math.abs(evaluateTemplateExpression(exp, obj))
+        : evaluateTemplateExpression(exp, obj)
+    );
+  }
+
+
+  console.log(`after parentheses: ${expression}`)
+
+  while (expression.match(binaryOpRegex("*/"))) {
+    expression = expression.replaceAll(binaryOpRegex("*/"), (_, t1, op, t2) =>
+      operators[op](parseTerm(t1, obj), parseTerm(t2, obj))
+    );
+  }
+
+  console.log(`after multiplication/division: ${expression}`)
+
+  while (expression.match(binaryOpRegex("+-"))) {
+    expression = expression.replaceAll(binaryOpRegex("+-"), (_, t1, op, t2) =>
+      operators[op](parseTerm(t1, obj), parseTerm(t2, obj))
+    );
+  }
+
+  console.log(`after addition/subtraction: ${expression}`)
+
+  return parseTerm(expression, obj);
+}
+
+export function replaceTemplateVars(string, obj={}) {
+  return string.replaceAll(templateRegex, (_, group) =>
+    evaluateTemplateExpression(group, obj)
+  );
+}
