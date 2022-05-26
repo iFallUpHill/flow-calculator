e14f0c62d9148e441da1c49e0e188339d9ae7ce4
Jason Manson-Hing <jason.mansonhing@gmail.com>
2022-05-26 00:50:24 -0400

    fixed overflow in sidebar

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ function App() {
 
   return (
   <div className="flex bg-slate-50">
-    <aside className="h-screen sticky top-0 w-96 bg-white drop-shadow">
-      <div className="m-8">
-        <h1 className="text-3xl mb-2 font-bold">Flow Test Generator</h1>
-        <section>
-          <h2 className="text-xl mb-2 font-bold">GCode Preview</h2>
-          <GCodePreview />
-        </section>
-        
-        <section className="mt-4">
-          <GCodeDownload />
-        </section>
+    <aside className="h-screen sticky top-0 w-96 bg-white drop-shadow overflow-y-scroll shrink-0">
+        <div className="m-8">
+          <h1 className="text-3xl mb-2 font-bold">Flow Test Generator</h1>
+          <section>
+            <h2 className="text-xl mb-2 font-bold">GCode Preview</h2>
+            <GCodePreview />
+          </section>
+          
+          <section className="mt-4">
+            <GCodeDownload />
+          </section>
 
-        <section className="mt-4">
-          <Credits />
-        </section>
-      </div>
+          <section className="mt-4">
+            <Credits />
+          </section>
+        </div>
     </aside>
     <main>
       <div className="m-8">
