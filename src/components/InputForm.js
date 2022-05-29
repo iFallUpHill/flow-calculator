f8bb78624139ed5f2eeb0a72ac69b5a46d279602
Jason Manson-Hing <jason.mansonhing@gmail.com>
2022-05-29 05:19:20 -0400

    cleaned up punctuation + lint

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -10,7 +10,6 @@ function InputForm() {
   const options = useStore((state) => state.options);
   const setOptions = useStore((state) => state.setOptions);
 
-  const showVariableNames = useStore((state) => state.showVariableNames);
   const setShowVariableNames = useStore((state) => state.setShowVariableNames);
 
   const startGcodeError = useStore((state) => state.startGcodeError);
@@ -68,17 +67,17 @@ function InputForm() {
         <Input type="number" value="bedWidth" label="Bed Width" unit="mm" hasVariable
         description="Width of print bed (i.e. X-axis length)"
         register={register("bedWidth", { required: true, valueAsNumber: true, validate: (value) => (value >= 100 && value <= 500)})}/>
-        {errors.bedWidth && <Error msg="Enter a valid bed width"/>}
+        {errors.bedWidth && <Error msg="Enter a valid bed width."/>}
 
         <Input type="number" value="bedLength" label="Bed Length" unit="mm" hasVariable
         description="Length of print bed (i.e. Y-axis length)"
         register={register("bedLength", { required: true, valueAsNumber: true, validate: (value) => (value >= 100 && value <= 500)})}/>
-        {errors.bedLength && <Error msg="Enter a valid bed length"/>}
+        {errors.bedLength && <Error msg="Enter a valid bed length."/>}
 
         <Input type="number" value="bedMargin" label="Bed Margin" unit="mm" hasVariable
         description="Safe distance from edge of bed to print on"
         register={register("bedMargin", { required: true, valueAsNumber: true, validate: (value) => (value >= 0 && value <= 50)})}/>
-        {errors.bedMargin && <Error msg="Enter a valid bed margin"/>}
+        {errors.bedMargin && <Error msg="Enter a valid bed margin."/>}
 
         <Select value="direction" label="Generation Direction" register={register("direction", { required: true, valueAsNumber: true })}
         description="For toolheads with large forward clearance"
@@ -102,16 +101,17 @@ function InputForm() {
         <Input type="number" value="travelSpeed" label="Travel Speed" unit="mm/s"
         description="Speed for non-printing moves"
         register={register("travelSpeed", { required: true, valueAsNumber: true, validate: (value) => (value >= 1 && value <= 2000)})}/>
+        {errors.travelSpeed && <Error msg="Enter a valid travel speed."/>}
 
         <Input type="number" value="bedTemp" label="Bed Temperature" unit="°C" hasVariable
         description="Temperature of print bed"
         register={register("bedTemp", { required: true, valueAsNumber: true, validate: (value) => (value >= 40 && value <= 150)})}/>
-        {errors.bedTemp && <Error msg="Enter a valid bed temperature"/>}
+        {errors.bedTemp && <Error msg="Enter a valid bed temperature."/>}
 
         <Input type="number" value="fanSpeed" label="Fan Speed" unit="%"  hasVariable
         description="Speed of part cooling fan"
         register={register("fanSpeed", { required: true, valueAsNumber: true, setValueAs: v => Math.round(v*255/100), validate: (value) => (value >= 0 && value <= 100)})}/>
-        {errors.fanSpeed && <Error msg="Enter a valid fan speed"/>}
+        {errors.fanSpeed && <Error msg="Enter a valid fan speed."/>}
       </div>
       
 
@@ -120,12 +120,12 @@ function InputForm() {
         <Input type="number" step={0.01} value="retractionDistance" label="Retraction Distance" unit="mm"
         description="Length of filament to recede during retraction"
         register={register("retractionDistance", { required: true, valueAsNumber: true, validate: (value) => (value >= 0 && value <= 10)})}/>
-        {errors.retractionDistance && <Error msg="Enter a valid retraction distance"/>}
+        {errors.retractionDistance && <Error msg="Enter a valid retraction distance."/>}
 
         <Input type="number" value="retractionSpeed" label="Retraction Speed" unit="mm/s"
         description="Speed that extruder drives back filament"
         register={register("retractionSpeed", { required: true, valueAsNumber: true, validate: (value) => (value >= 1 && value <= 50)})}/>
-        {errors.retractionSpeed && <Error msg="Enter a valid retraction speed"/>}
+        {errors.retractionSpeed && <Error msg="Enter a valid retraction speed."/>}
       </div>
 
       <h2 className="text-lg mt-4 font-bold">Pattern Configuration</h2>
@@ -133,17 +133,17 @@ function InputForm() {
         <Input type="number" value="flowSpacing" label="Flow Spacing" unit="mm"
         description="Space between flow tests (i.e. Y-spacing)"
         register={register("flowSpacing", { required: true, valueAsNumber: true, validate: (value) => (value >= 25 && value <= 500)})}/>
-        {errors.flowSpacing && <Error msg="Enter a valid flow spacing"/>}
+        {errors.flowSpacing && <Error msg="Enter a valid flow spacing."/>}
 
         <Input type="number" value="tempSpacing" label="Temperature Spacing" unit="mm"
         description="Space between temperature tests (i.e. X-spacing)"
         register={register("tempSpacing", { required: true, valueAsNumber: true, validate: (value) => (value >= 25 && value <= 500)})}/>
-        {errors.tempSpacing && <Error msg="Enter a valid temperature spacing"/>}
+        {errors.tempSpacing && <Error msg="Enter a valid temperature spacing."/>}
 
         <Input type="number" value="dwellTime" label="Dwell Time" unit="s"
         description="Pause before each prime line"
         register={register("dwellTime", { required: true, valueAsNumber: true, validate: (value) => (value >= 0 && value <= 30)})}/>
-        {errors.dwellTime && <Error msg="Enter a valid dwell time"/>}
+        {errors.dwellTime && <Error msg="Enter a valid dwell time."/>}
       </div>
 
       <h2 className="text-lg mt-4 font-bold">Flow Test (Rows)</h2>
@@ -151,19 +151,19 @@ function InputForm() {
         <Input type="number" value="flowStart" label="Start Flow" unit="mm³/s" 
         description="Requested flow rate for first flow test in sequence"
         register={register("flowStart", { required: true, valueAsNumber: true, validate: (value) => (value >= 2 && value <= 100)})}/>
-        {errors.flowStart && <Error msg="Enter a valid start flow"/>}
+        {errors.flowStart && <Error msg="Enter a valid start flow."/>}
 
         <Input type="number" value="flowOffset" label="Offset Flow" unit="mm³/s" 
         description="Change in requested flow rate between flow steps"
         register={register("flowOffset", { required: true, valueAsNumber: true, validate: (value) => (value >= 0 && value <= 30)})}/>
-        {errors.flowOffset && <Error msg="Enter a valid offset flow"/>}
+        {errors.flowOffset && <Error msg="Enter a valid offset flow."/>}
 
         <Input type="number" value="flowSteps" label="Flow Steps" 
         description="Inclusive; Number of flow steps to test"
         register={register("flowSteps", { required: true, valueAsNumber: true, validate: (value) => (value >= 1 && value <= 40)})}/>
-        {errors.flowSteps && <Error msg="Enter a valid number of flow steps"/>}
-        {options.tempSteps > 1 && !errors.flowSteps && options.flowSteps > validMaxFlowStepsPerColumn() && <Warning msg="Requested flow steps exceed bed length"/>}
-        {options.tempSteps === 1 && !errors.flowSteps && options.flowSteps > (validMaxFlowStepsPerColumn() * validMaxTempSteps()) && <Warning msg="Requested flow steps exceed bed size"/>}
+        {errors.flowSteps && <Error msg="Enter a valid number of flow steps."/>}
+        {options.tempSteps > 1 && !errors.flowSteps && options.flowSteps > validMaxFlowStepsPerColumn() && <Warning msg="Requested flow steps exceed bed length."/>}
+        {options.tempSteps === 1 && !errors.flowSteps && options.flowSteps > (validMaxFlowStepsPerColumn() * validMaxTempSteps()) && <Warning msg="Requested flow steps exceed bed size."/>}
 
         <Input type="number" defaultValue={options.flowEnd} label="End Flow" unit="mm³/s" disabled={true} 
         description="Requested flow rate for last flow test in sequence"/>
@@ -175,18 +175,18 @@ function InputForm() {
         <Input type="number" value="tempStart" label="Start Temperature" unit="°C" hasVariable
         description="Hotend temperature for first temperature column"
         register={register("tempStart", { required: true, valueAsNumber: true, validate: (value) => (value >= 155 && value <= 450)})}/>
-        {errors.tempStart && <Error msg="Enter a valid start temperature"/>}
+        {errors.tempStart && <Error msg="Enter a valid start temperature."/>}
 
         <Input type="number" value="tempOffset" label="Offset Temperature" unit="°C"
         description="Change in temperature between temperature columns"
         register={register("tempOffset", { required: true, valueAsNumber: true, validate: (value) => (value >= 0 && value <= 115)})}/>
-        {errors.tempOffset && <Error msg="Enter a valid offset temperature"/>}
+        {errors.tempOffset && <Error msg="Enter a valid offset temperature."/>}
 
         <Input type="number" value="tempSteps" label="Temperature Steps" 
         description="Inclusive; Set to 1 to fill plate with flow steps instead"
         register={register("tempSteps", { required: true, valueAsNumber: true, validate: (value) => (value >= 1 && value <= 10)})}/>
-        {errors.tempSteps && <Error msg="Enter a valid number of temperature steps"/>}
-        {options.tempSteps !== 1 && !errors.tempSteps && options.tempSteps > validMaxTempSteps() && <Warning msg="Requested temp steps exceed bed width"/>}
+        {errors.tempSteps && <Error msg="Enter a valid number of temperature steps."/>}
+        {options.tempSteps !== 1 && !errors.tempSteps && options.tempSteps > validMaxTempSteps() && <Warning msg="Requested temp steps exceed bed width."/>}
 
 
         <Input type="number" defaultValue={options.tempEnd} label="End Temperature" unit="°C" disabled={true} 
@@ -203,7 +203,7 @@ function InputForm() {
           <Input type="number" value="safeZPark" label="Safe Z Park Height" unit="mm" hasVariable
           description="Absolute position; Safe height to park toolhead"
           register={register("safeZPark", { required: true, valueAsNumber: true, validate: (value) => (value >= 1 && value <= 50)})}/>
-          {errors.safeZPark && <Error msg="Enter a safe z park height"/>}
+          {errors.safeZPark && <Error msg="Enter a valide safe z park height."/>}
         
           <Select value="showVariableNames" label="Show Variable Names"
             description="For use in custom start and end Gcode"
@@ -217,11 +217,13 @@ function InputForm() {
 
 
         <TextArea value="startGcode" label="Start Gcode" 
+        // eslint-disable-next-line no-template-curly-in-string
         description="Variables within ${variable} will be evaluated. +, -, *, /, and Math.abs(variable) are also supported."
         register={register("startGcode")}/>
         {startGcodeError && <Error msg={startGcodeError} />}
 
         <TextArea value="endGcode" label="End Gcode" 
+        // eslint-disable-next-line no-template-curly-in-string
         description="Variables within ${variable} will be evaluated. +, -, *, /, and Math.abs(variable) are also supported."
         register={register("endGcode")}/>
         {endGcodeError && <Error msg={endGcodeError} />}
@@ -235,32 +237,32 @@ function InputForm() {
         <Input type="number" value="primeLength" label="Prime Line Length" unit="mm"
         description="Length of prime line before blob"
         register={register("primeLength", { required: true, valueAsNumber: true, validate: (value) => (value >= 1 && value <= 50)})}/>
-        {errors.primeLength && <Error msg="Enter a valid prime line length"/>}
+        {errors.primeLength && <Error msg="Enter a valid prime line length."/>}
 
         <Input type="number" value="primeAmount" label="Prime Amount" unit="mm"
         description="Length of filament to extrude for prime line"
         register={register("primeAmount", { required: true, valueAsNumber: true, validate: (value) => (value >= 1 && value <= 50)})}/>
-        {errors.primeAmount && <Error msg="Enter a valid length to extrude"/>}
+        {errors.primeAmount && <Error msg="Enter a valid length to extrude."/>}
 
         <Input type="number" value="primeSpeed" label="Prime Speed" unit="mm/s"
         description="Speed of the prime move"
         register={register("primeSpeed", { required: true, valueAsNumber: true, validate: (value) => (value >= 1 && value <= 50)})}/>
-        {errors.primeSpeed && <Error msg="Enter a valid prime speed"/>}
+        {errors.primeSpeed && <Error msg="Enter a valid prime speed."/>}
 
         <Input type="number" value="wipeLength" label="Wipe Length" unit="mm"
         description="Distance between prime line end and blob center"
         register={register("wipeLength", { required: true, valueAsNumber: true, validate: (value) => (value >= 1 && value <= 50)})}/>
-        {errors.wipeLength && <Error msg="Enter a valid wipe length"/>}
+        {errors.wipeLength && <Error msg="Enter a valid wipe length."/>}
 
         <Input type="number" value="blobHeight" label="Blob Height" unit="mm"
         description="Height of printed blob"
         register={register("blobHeight", { required: true, valueAsNumber: true, validate: (value) => (value >= 1 && value <= 50)})}/>
-        {errors.blobHeight && <Error msg="Enter a valid blob height"/>}
+        {errors.blobHeight && <Error msg="Enter a valid blob height."/>}
 
         <Input type="number" value="extrusionAmount" label="Extrusion Amount" unit="mm"
         description="Length of filament to extrude into blob"
         register={register("extrusionAmount", { required: true, valueAsNumber: true, validate: (value) => (value >= 100 && value <= 500)})}/>
-        {errors.extrusionAmount && <Error msg="Enter a valid extrusion amount"/>}
+        {errors.extrusionAmount && <Error msg="Enter a valid extrusion amount."/>}
       </div>
     </form>
   );
