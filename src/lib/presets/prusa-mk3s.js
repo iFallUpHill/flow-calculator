6f97eb31847d760ea4ff6c74b03fcf7b6c6a0d57
Jason Manson-Hing <jason.mansonhing@gmail.com>
2022-06-09 03:36:05 -0400

    persist settings to localstorage

---
+++ src/lib/presets/prusa-mk3s.js
@@ -0,0 +1,45 @@
+// For a stock Prusa MK3/S/+
+export const prusaMK3SDefaults = {
+    bedWidth: 250, // mm
+    bedLength: 210, // mm
+    bedMargin: 5, // mm 
+    safeZPark: 10, // mm
+    filamentDiameter: 1.75, // mm
+    travelSpeed: 100, // mm/s
+    dwellTime: 20, // s
+    bedTemp: 60, // °C
+    fanSpeed: 0, // % * 100
+    primeLength: 25, // mm
+    primeAmount: 20, // mm
+    primeSpeed: 5, // mm/s
+    wipeLength: 15, // mm
+    retractionDistance: 0.8, // mm
+    retractionSpeed: 35, //mm/s
+    blobHeight: 10, // mm 
+    extrusionAmount: 200, //mm
+    direction: 1, // 1 = front to back, -1 = back to front
+    flowSpacing: 50, // mm
+    tempSpacing: 38, // mm
+    flowStart: 8, // mm^3/s
+    flowOffset: 2, // mm^3/s
+    flowSteps: 4, // unitless, (end-start)/offset
+    flowEnd: 14, // mm^3/s
+    tempStart: 200, // °C
+    tempOffset: 20, // °C
+    tempSteps: 3, // unitless, (end-start)/offset
+    tempEnd: 240, // °C
+    startGcode: `M104 S\${tempStart} ; Set Nozzle Temperature
+M140 S\${bedTemp} ; Set Bed Temperature
+G90 ; Absolute positioning
+G28 ; Move to home position
+G0 Z\${safeZPark} ; Lift nozzle
+G21 ; unit in mm
+G92 E0 ; reset extruder
+M83 ; set extruder to relative mode
+M190 S\${bedTemp} ; Wait for Bed Temperature
+M106 S\${fanSpeed} ; Set Fan Speed`, 
+endGcode: `G0 X\${bedWidth - bedMargin} Y\${bedLength - bedMargin} ; Move to Corner
+M104 S0 T0 ; Turn Off Hotend
+M140 S0 ; Turn Off Bed
+M84 ; Disable Steppers`, 
+}
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,57 +1,24 @@
-import create from "zustand";
+import create from 'zustand';
+import { prusaMK3SDefaults } from '../lib/presets/prusa-mk3s';
+import { version } from '../lib/version';
+import { persist } from 'zustand/middleware';
 
-export const useStore = create((set) => ({
-    options: {
-        bedWidth: 250, // mm
-        bedLength: 210, // mm
-        bedMargin: 5, // mm 
-        safeZPark: 10, // mm
-        filamentDiameter: 1.75, // mm
-        travelSpeed: 100, // mm/s
-        dwellTime: 20, // s
-        bedTemp: 60, // °C
-        fanSpeed: 0, // % * 100
-        primeLength: 25, // mm
-        primeAmount: 20, // mm
-        primeSpeed: 5, // mm/s
-        wipeLength: 15, // mm
-        retractionDistance: 0.8, // mm
-        retractionSpeed: 35, //mm/s
-        blobHeight: 10, // mm 
-        extrusionAmount: 200, //mm
-        direction: 1, // 1 = front to back, -1 = back to front
-        flowSpacing: 50, // mm
-        tempSpacing: 38, // mm
-        flowStart: 8, // mm^3/s
-        flowOffset: 2, // mm^3/s
-        flowSteps: 4, // unitless, (end-start)/offset
-        flowEnd: 14, // mm^3/s
-        tempStart: 200, // °C
-        tempOffset: 20, // °C
-        tempSteps: 3, // unitless, (end-start)/offset
-        tempEnd: 240, // °C
-        startGcode: `M104 S\${tempStart} ; Set Nozzle Temperature
-M140 S\${bedTemp} ; Set Bed Temperature
-G90 ; Absolute positioning
-G28 ; Move to home position
-G0 Z\${safeZPark} ; Lift nozzle
-G21 ; unit in mm
-G92 E0 ; reset extruder
-M83 ; set extruder to relative mode
-M190 S\${bedTemp} ; Wait for Bed Temperature
-M106 S\${fanSpeed} ; Set Fan Speed`, 
-        endGcode: `G0 X\${bedWidth - bedMargin} Y\${bedLength - bedMargin} ; Move to Corner
-M104 S0 T0 ; Turn Off Hotend
-M140 S0 ; Turn Off Bed
-M84 ; Disable Steppers`, 
-    },
-    setOptions: (options) => set(() => ({ options: options })),
-    fileName: '',
-    setFileName: (fileName) => set(() => ({ fileName: fileName })),
-    showVariableNames: false,
-    setShowVariableNames: (showVariableNames) => set((state) => ({ showVariableNames: !state.showVariableNames })),
-    startGcodeError: '',
-    setStartGcodeError: (startGcodeError) => set(() => ({ startGcodeError: startGcodeError })),
-    endGcodeError: '',
-    setEndGcodeError: (endGcodeError) => set(() => ({ endGcodeError: endGcodeError })),
-}));
+export const useStore = create(
+    persist((set,get) => ({
+        options: prusaMK3SDefaults,
+        setOptions: (options) => set({ options: options }),
+        fileName: '',
+        setFileName: (fileName) => set({ fileName: fileName }),
+        showVariableNames: false,
+        setShowVariableNames: () => set({ showVariableNames: !get().showVariableNames }),
+        startGcodeError: '',
+        setStartGcodeError: (startGcodeError) => set({ startGcodeError: startGcodeError }),
+        endGcodeError: '',
+        setEndGcodeError: (endGcodeError) => set({ endGcodeError: endGcodeError }),
+    }),
+    {
+        name: 'generator-parameters',
+        version: version,
+        getStorage: () => localStorage,
+    }
+));
